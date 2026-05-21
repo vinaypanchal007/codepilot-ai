cc6539e7ccff82d7d051ecc01587e8258569861a
vinaypanchal007 <vinayp.12601@gmail.com>
2026-05-21 14:43:28 +0530

    Added new files

--- rag.py
+++ rag.py
@@ -12,3 +12,19 @@ from config import (
     REPOSITORIES,
     BASE_DIR
 )
+
+def main():
+    print("Starting  respository ingestion process...")
+    
+    for repo in REPOSITORIES:
+        repo = repo.strip()
+        clone_repository(repo)
+        repo_path = os.path.join(BASE_DIR, repo)
+        files = read_files(repo_path)
+        
+        print(f"Total files read from {repo}: {len(files)}")
+    
+    print("Repository ingestion process completed.")
+    
+if __name__ == "__main__":
+    main()
